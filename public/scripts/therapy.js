cf24b1d1ba43d20a57d0ff0d02497a2c549b9866
Francesca Ansell <fla5029@psu.edu>
2020-11-18 17:32:14 -0500

    Fix Image Heights and Book Buttons

--- public/scripts/therapy.js
+++ public/scripts/therapy.js
@@ -1,8 +1,8 @@
-console.log("Ready");
-
 const rootURL = "https://api.timekit.io/v2"
 
-document.getElementById("book").addEventListener("click", display);
+document.getElementById("book1").addEventListener("click", display);
+document.getElementById("book2").addEventListener("click", display);
+document.getElementById("book3").addEventListener("click", display);
 
 function display(){
     document.getElementById("bookingjs").style.display = "block";
