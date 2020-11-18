1faa8ebdb0d44af66bfa7264a3782e8e74d0b387
Elissa <elissa@Elissas-MacBook-Pro.local>
2020-11-18 17:35:19 -0500

    update faq

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,6 +1,6 @@
 console.log("Ready");
 
-
+// THERAPY PAGE 
 const rootURL = "https://api.timekit.io/v2"
 
 document.getElementById("bookingjs").style.display =  "none";
@@ -12,3 +12,4 @@ document.getElementById("book").addEventListener("click", display);
 function display(){
     document.getElementById("bookingjs").style.display = "block";
 }
+
