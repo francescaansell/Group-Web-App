637ac949a610190f93dd011bb9ee455b4fc180ff
Francesca Ansell <fla5029@psu.edu>
2020-11-18 15:10:37 -0500

    Fix Survey page/Review Page

---
+++ public/scripts/survey.js
@@ -0,0 +1,3 @@
+console.log("Survey");
+
+document.getElementById("Submit").addEventListener("click", function () { alert("Your information has been recorded you will recieve an email when you have been matched with a therapist.") } )
--- public/scripts/therapy.js
+++ public/scripts/therapy.js
@@ -1,13 +1,10 @@
 console.log("Ready");
 
-
 const rootURL = "https://api.timekit.io/v2"
 
 document.getElementById("bookingjs").style.display =  "none";
 
 document.getElementById("book").addEventListener("click", display);
-;
-
 
 function display(){
     document.getElementById("bookingjs").style.display = "block";
