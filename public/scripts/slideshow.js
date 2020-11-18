8fe2386726949dfc6db3740952e250ef1a451450
Francesca Ansell <fla5029@psu.edu>
2020-11-18 17:19:10 -0500

    Index slideshow

--- public/scripts/slideshow.js
+++ public/scripts/slideshow.js
@@ -1,5 +1,11 @@
+//Resource https://www.w3schools.com/howto/howto_js_slideshow.asp
+console.log("Check");
+/*
+
 const slideShowSlide = document.querySelector('.slideshow-slide');
 const slideShowImages = document.querySelectorAll('.slideshow-slide img');
+console.log(slideShowImages);
+console.log(document.querySelectorAll('.slideshow-slide img'));
 const slideShowText = document.querySelectorAll('slideshow-slide p');
 
 //Buttons
@@ -39,3 +45,20 @@ slideShowSlide.addEventListener('transitionend', function (ease){
         slideShowSlide.style.transform = "translateX(" +  (-size * counter) + 'px)';
     }
 });
+
+*/
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 4000); 
+}
