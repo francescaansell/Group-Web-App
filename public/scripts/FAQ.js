d6aa48661e0b8e7657dddf68eafc297579ae5d06
Elissa <elissa@Elissas-MacBook-Pro.local>
2020-11-18 20:09:22 -0500

    more FAQ updates

--- public/scripts/FAQ.js
+++ public/scripts/FAQ.js
@@ -17,18 +17,16 @@ const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", questionForm);
 
 
-
-
 // FROM EXAMPLE 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("dropDownInfo").classList.toggle("show");
   }
   
-  // Close the dropdown menu if the user clicks outside of it
+  // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropDownBtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
