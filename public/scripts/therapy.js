6b84f84fc2757bc7cabfb634b42b6d6781cbd8d3
Francesca Ansell <55817398+francescaansell@users.noreply.github.com>
2020-11-18 20:01:44 -0500

    Jquery

--- public/scripts/therapy.js
+++ public/scripts/therapy.js
@@ -1,8 +1,8 @@
 const rootURL = "https://api.timekit.io/v2"
 
-document.getElementById("book1").addEventListener("click", display);
-document.getElementById("book2").addEventListener("click", display);
-document.getElementById("book3").addEventListener("click", display);
+$("#book1").click(display);
+$("#book2").click(display);
+$("#book3").click(display);
 
 function display(){
     document.getElementById("bookingjs").style.display = "block";
